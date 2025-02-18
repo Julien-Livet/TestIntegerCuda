b54fd8bbb8b15d94bbb4ab279e3400deeddf8b0d
Julien <76999467+Julien-Livet@users.noreply.github.com>
2025-02-18 13:03:32 +0100

    Add files via upload

--- main.cu
+++ main.cu
@@ -4,7 +4,7 @@
 
 int main()
 {
-    using Z = Integer<unsigned long long, thrust::host_vector<unsigned long long> >;
+    using Z = Integer<unsigned long long, std::vector<unsigned long long> >;
 
     std::string const s("56062005704198360319209");
     Z const n(s);
