d25cf6eb79ad74d6244171ec853e5c400ab48184
Julien <76999467+Julien-Livet@users.noreply.github.com>
2025-02-19 16:24:34 +0100

    Add files via upload

--- make_TestIntegerCuda.bash
+++ make_TestIntegerCuda.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
-#nvcc -rdc=true --std c++20 -v -Xcompiler -Wall -w -Xcompiler -v --ptxas-options=-v main.cu -o TestIntegerCuda
-nvcc -rdc=true --std c++20 -Xcompiler -Wall -w main.cu -o TestIntegerCuda
+#nvcc --expt-relaxed-constexpr -rdc=true --std c++20 -v -Xcompiler -Wall -w -Xcompiler -v --ptxas-options=-v main.cu -o TestIntegerCuda
+#nvcc --expt-relaxed-constexpr -rdc=true --std c++20 -Xcompiler -Wall -w main.cu -o TestIntegerCuda
+nvcc --expt-relaxed-constexpr -rdc=true --std c++20 -Xcompiler -Wall main.cu -o TestIntegerCuda
