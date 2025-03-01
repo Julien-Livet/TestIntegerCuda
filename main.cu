d7733a135ef18ccc050a9937c29eb268ff4469b9
Julien <76999467+Julien-Livet@users.noreply.github.com>
2025-03-01 17:22:46 +0100

    Add files via upload

--- main.cu
+++ main.cu
@@ -13,28 +13,23 @@ __global__ void isPrime(T const* nData, size_t nDataSize, unsigned int const* p,
 int main()
 {
     unsigned int* p(nullptr);
-    auto r{cudaMalloc(&p, sizeof(unsigned int) * primes.size())};
-    assert(r == cudaSuccess);
+    gpuErrchk(cudaMalloc(&p, sizeof(unsigned int) * primes.size()));
     assert(p);
-    r = cudaMemcpy(p, primes.data(), sizeof(unsigned int) * primes.size(), cudaMemcpyHostToDevice);
-    assert(r == cudaSuccess);
+    gpuErrchk(cudaMemcpy(p, primes.data(), sizeof(unsigned int) * primes.size(), cudaMemcpyHostToDevice));
 
     int* prime(nullptr);
-    r = cudaMalloc(&prime, sizeof(int));
-    assert(r == cudaSuccess);
+    gpuErrchk(cudaMalloc(&prime, sizeof(int)));
     assert(prime);
 
-    r = cudaDeviceSetLimit(cudaLimitStackSize, 256 * 256);
-    assert(r == cudaSuccess);
+    gpuErrchk(cudaDeviceSetLimit(cudaLimitStackSize, 256 * 256));
 
-    r = cudaDeviceSetLimit(cudaLimitMallocHeapSize, 256 * 256);
-    assert(r == cudaSuccess);
+    gpuErrchk(cudaDeviceSetLimit(cudaLimitMallocHeapSize, 1024 * 1024));
     
     using T = uintmax_t;
     
     {
         std::cout << "Block #1" << std::endl;
-        cudaMemset(prime, -1, sizeof(int));
+        gpuErrchk(cudaMemset(prime, -1, sizeof(int)));
 
         auto const n(23 * 29_z);
 
@@ -43,30 +38,27 @@ int main()
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
         T* nData(nullptr);    
-        r = cudaMalloc(&nData, sizeof(T) * n.bits().size());
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMalloc(&nData, sizeof(T) * n.bits().size()));
         assert(nData);
-        r = cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice));
         
         t = std::chrono::steady_clock::now();
         isPrime<T><<<1, 1>>>(nData, n.bits().size(), p, primes.size(), prime);
 
-        cudaDeviceSynchronize();
+        gpuErrchk(cudaDeviceSynchronize());
 
         int pr(-1);
-        r = cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost));
 
         std::cout << pr << std::endl;
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
-        cudaFree(nData);
+        gpuErrchk(cudaFree(nData));
     }
 
     {
         std::cout << "Block #2" << std::endl;
-        cudaMemset(prime, -1, sizeof(int));
+        gpuErrchk(cudaMemset(prime, -1, sizeof(int)));
 
         auto const n(1299709 * 1299721_z);
 
@@ -75,30 +67,27 @@ int main()
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
         T* nData(nullptr);    
-        r = cudaMalloc(&nData, sizeof(T) * n.bits().size());
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMalloc(&nData, sizeof(T) * n.bits().size()));
         assert(nData);
-        r = cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice));
         
         t = std::chrono::steady_clock::now();
         isPrime<T><<<1, 1>>>(nData, n.bits().size(), p, primes.size(), prime);
 
-        cudaDeviceSynchronize();
+        gpuErrchk(cudaDeviceSynchronize());
 
         int pr(-1);
-        r = cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost));
 
         std::cout << pr << std::endl;
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
-        cudaFree(nData);
+        gpuErrchk(cudaFree(nData));
     }
 
     {
         std::cout << "Block #3" << std::endl;
-        cudaMemset(prime, -1, sizeof(int));
+        gpuErrchk(cudaMemset(prime, -1, sizeof(int)));
     
         auto const n(56062005704198360319209_z);
 
@@ -107,58 +96,52 @@ int main()
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
         T* nData(nullptr);    
-        r = cudaMalloc(&nData, sizeof(T) * n.bits().size());
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMalloc(&nData, sizeof(T) * n.bits().size()));
         assert(nData);
-        r = cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice));
         
         t = std::chrono::steady_clock::now();
         isPrime<T><<<1, 1>>>(nData, n.bits().size(), p, primes.size(), prime);
 
-        cudaDeviceSynchronize();
+        gpuErrchk(cudaDeviceSynchronize());
 
         int pr(-1);
-        r = cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost));
 
         std::cout << pr << std::endl;
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
-        cudaFree(nData);
+        gpuErrchk(cudaFree(nData));
     }
 
     {
         std::cout << "Block #4" << std::endl;
-        cudaMemset(prime, -1, sizeof(int));
+        gpuErrchk(cudaMemset(prime, -1, sizeof(int)));
 
         auto const n(4113101149215104800030529537915953170486139623539759933135949994882770404074832568499_z);
 
         T* nData(nullptr);    
-        r = cudaMalloc(&nData, sizeof(T) * n.bits().size());
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMalloc(&nData, sizeof(T) * n.bits().size()));
         assert(nData);
-        r = cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(nData, n.bits().data(), sizeof(T) * n.bits().size(), cudaMemcpyHostToDevice));
         
         auto const t{std::chrono::steady_clock::now()};
         isPrime<T><<<1, 1>>>(nData, n.bits().size(), p, primes.size(), prime);
 
-        cudaDeviceSynchronize();
+        gpuErrchk(cudaDeviceSynchronize());
 
         int pr(-1);
-        r = cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost));
 
         std::cout << pr << std::endl;
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
-        cudaFree(nData);
+        gpuErrchk(cudaFree(nData));
     }
 
     {
         std::cout << "Block #5" << std::endl;
-        cudaMemset(prime, -1, sizeof(int));
+        gpuErrchk(cudaMemset(prime, -1, sizeof(int)));
 
         Integer64 n;
         n.setPrecision(1024 / 64);
@@ -167,32 +150,27 @@ int main()
         if (!(n % 2))
             ++n;
 
-            uint64_t* nData(nullptr);    
-        r = cudaMalloc(&nData, sizeof(uint64_t) * n.bits().size());
-        assert(r == cudaSuccess);
+        uint64_t* nData(nullptr);    
+        gpuErrchk(cudaMalloc(&nData, sizeof(uint64_t) * n.bits().size()));
         assert(nData);
-        r = cudaMemcpy(nData, n.bits().data(), sizeof(uint64_t) * n.bits().size(), cudaMemcpyHostToDevice);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(nData, n.bits().data(), sizeof(uint64_t) * n.bits().size(), cudaMemcpyHostToDevice));
         
         auto const t{std::chrono::steady_clock::now()};
         isPrime<uint64_t><<<1, 1>>>(nData, n.bits().size(), p, primes.size(), prime);
 
-        cudaDeviceSynchronize();
+        gpuErrchk(cudaDeviceSynchronize());
 
         int pr(-1);
-        r = cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost);
-        assert(r == cudaSuccess);
+        gpuErrchk(cudaMemcpy(&pr, prime, sizeof(int), cudaMemcpyDeviceToHost));
 
         std::cout << pr << std::endl;
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t).count() << " ms" << std::endl;
 
-        cudaFree(nData);
+        gpuErrchk(cudaFree(nData));
     }
 
-    r = cudaFree(p);
-    assert(r == cudaSuccess);
-    r = cudaFree(prime);
-    assert(r == cudaSuccess);
+    gpuErrchk(cudaFree(p));
+    gpuErrchk(cudaFree(prime));
 
     return 0;
 }
