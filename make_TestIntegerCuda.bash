84f2c1fcad29ee54b4129dbd1496ac699358989f
Julien <76999467+Julien-Livet@users.noreply.github.com>
2025-03-03 19:15:32 +0100

    Add files via upload

--- make_TestIntegerCuda.bash
+++ make_TestIntegerCuda.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Release
-#nvcc -D__CDPRT_SUPPRESS_SYNC_DEPRECATION_WARNING -DCUDA_FORCE_CDP1_IF_SUPPORTED -diag-suppress 23,63,2361 --expt-relaxed-constexpr -rdc=true --std c++20 -DNDEBUG -Xcompiler -Wall main.cu -o TestIntegerCuda
+#nvcc -D__CDPRT_SUPPRESS_SYNC_DEPRECATION_WARNING -DCUDA_FORCE_CDP1_IF_SUPPORTED -diag-suppress 23,63,2361,20012 --expt-relaxed-constexpr -rdc=true --std c++20 -DNDEBUG -Xcompiler -Wall main.cu -o TestIntegerCuda
 #Debug
-nvcc -D__CDPRT_SUPPRESS_SYNC_DEPRECATION_WARNING -DCUDA_FORCE_CDP1_IF_SUPPORTED -G -g -diag-suppress 23,63,2361 --expt-relaxed-constexpr -rdc=true --std c++20 -Xcompiler -Wall main.cu -o TestIntegerCuda
+nvcc -D__CDPRT_SUPPRESS_SYNC_DEPRECATION_WARNING -DCUDA_FORCE_CDP1_IF_SUPPORTED -G -g -diag-suppress 23,63,2361,20012 --expt-relaxed-constexpr -rdc=true --std c++20 -Xcompiler -Wall main.cu -o TestIntegerCuda
